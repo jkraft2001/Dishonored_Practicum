6596a2974989d27362f46aa16b0269f90a9c4b4e
JKrafty <kraft.jackson@gmail.com>
2024-02-26 17:17:18 -0600

    Added Grenade
    Added input for spawning grenade
    Edited grenade code and made a blueprint actor derived from the grenade cpp class
    made the FP_Character able to spawn in the grenade and activate a countdown.

--- Source/Dishonored_TechDemo/AGrenade.cpp
+++ Source/Dishonored_TechDemo/AGrenade.cpp
@@ -2,12 +2,33 @@
 
 
 #include "AGrenade.h"
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 AAGrenade::AAGrenade()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	MeshComp->SetSimulatePhysics(true);
+	RootComponent = MeshComp;
+
+	ForceComp = CreateDefaultSubobject<URadialForceComponent>("ForceComp");
+	ForceComp->SetupAttachment(MeshComp);
+
+	ForceComp->SetAutoActivate(false);
+
+	ForceComp->Radius = 500.0f;
+	ForceComp->ImpulseStrength = 2500.0f;
+	ForceComp->bImpulseVelChange = true;
+
+	ForceComp->AddCollisionChannelToAffect(ECC_WorldDynamic);
+
+	// Binding either here or in PostInitializeComponent() below
+	// MeshComp->OnComponentHit.AddDynamic(this, &AAGrenade::OnActorHit;
 
 }
 
@@ -25,3 +46,23 @@ void AAGrenade::Tick(float DeltaTime)
 
 }
 
+void AAGrenade::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	MeshComp->OnComponentHit.AddDynamic(this, &AAGrenade::OnActorHit);
+
+}
+
+void AAGrenade::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	ForceComp->FireImpulse();
+
+	UE_LOG(LogTemp, Log, TEXT("OnActorHit an Explosive Barrel"));
+
+	UE_LOG(LogTemp, Warning, TEXT("OtherActor: %s, at game time: %f"), *GetNameSafe(OtherActor), GetWorld()->TimeSeconds);
+
+	FString CombinedString = FString::Printf(TEXT("Hit at location: %s"), *Hit.ImpactPoint.ToString());
+	//DrawDebugSphere(GetWorld(), Hit.ImpactPoint, CombinedString, nullptr, FColor::Green, 2.0f, true);
+
+}
--- Source/Dishonored_TechDemo/AGrenade.h
+++ Source/Dishonored_TechDemo/AGrenade.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Actor.h"
 #include "AGrenade.generated.h"
 
+class UStaticMeshComponent;
+class URadialForceComponent;
+
 UCLASS()
 class DISHONORED_TECHDEMO_API AAGrenade : public AActor
 {
@@ -18,6 +21,17 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(BlueprintReadOnly)
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(BlueprintReadOnly)
+	URadialForceComponent* ForceComp;
+
+	virtual void PostInitializeComponents() override;
+
+	UFUNCTION()
+	void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 public:	
 	// Called every frame
